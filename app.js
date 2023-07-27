40fa45ce6d4b38cbd2ec8a4dbf1bee5db1cc81ef
Alejandro Torrejon <alejandro.cartografia@gmail.com>
2023-07-27 14:22:07 -0300

    criando pagina de contato e arrumando extensao js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 console.log("O Paulo fez esse commit sem você ver na aula")
+console.log("configuração do banco de dados")
